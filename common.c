4e885bbe0a1e48dba90dbfcbe52b5aff46c0947d
ozuheian <hanoiozu@gmail.com>
2018-11-16 11:04:29 +0100

    merge branch

--- common.c
+++ common.c
@@ -4,12 +4,7 @@
 #include "wiki.h"
 
 void printEntry(Entry e) {
-  printf("%lu: %s\n\t %s\n\n",
-                              e.id,
-                              e.title,
-                              e.content+(e.id?strlen(e.title)+2:0)
-                              //removing the title that starts the article
-                              );
+  printf("%lu: %s\n\t %s\n\n", e.id, e.title, e.content+(e.id?strlen(e.title)+2:0)); // removing the title that starts the article
 }
 
 Wiki loadFile(char* file) {
@@ -20,32 +15,23 @@ Wiki loadFile(char* file) {
   if (fp!=NULL) {
 
     char c = 0;
-    long start, size;
+    long start;
+    long i = 0;
     do {
       Entry e;
       fscanf(fp, "%lu", &(e.id));
 
       fgetc(fp);  //eating the first '|'
 
-      for (start = ftell(fp); c != '|' && c != EOF; c = getc(fp)){}
-      if (c != EOF) {
-        size = ftell(fp)-start;
-        e.title = calloc(size+1, sizeof(char));
-        fseek(fp, start, SEEK_SET);
-        fread(e.title, sizeof(char), size-1, fp);
-      } else {
-        e.id = 0;
-      }
-
-      for (start = ftell(fp); c != '\n' && c != EOF; c = getc(fp)){}
-      if (c != EOF) {
-        size = ftell(fp)-start;
-        e.content = calloc(size+1, sizeof(char));
-        fseek(fp, start, SEEK_SET);
-        fread(e.content, sizeof(char), size-1, fp);
-      } else {
-        e.id = 0;
-      }
+      for (i = ftell(fp), start = i; c != '|' && c != EOF; i++, c = getc(fp)){}
+      e.title = calloc(i-start, sizeof(char));
+      fseek(fp, start, SEEK_SET);
+      fread(e.title, sizeof(char), i-start-1, fp);
+      fgetc(fp); //eating the '|'
+      for (i = ftell(fp), start = i; c != '\n' && c != EOF; i++, c = getc(fp)){}
+      e.content = calloc(i-start, sizeof(char));
+      fseek(fp, start, SEEK_SET);
+      fread(e.content, sizeof(char), i-start, fp);
 
       wiki = insert(wiki, e);
 
@@ -56,22 +42,10 @@ Wiki loadFile(char* file) {
   return wiki;
 }
 
+
 void freeEntry(Entry e)
 {
-  if (e.id) {
-    free(e.content);
-    free(e.title);
-    memset(&e, 0, sizeof(Entry));
-  }
-}
-
-inline Entry copyEntry(Entry e)
-{
-  Entry r;
-  r.id = e.id;
-  r.title = malloc(sizeof(char)*(strlen(e.title)+1));
-  strcpy(r.title, e.title);
-  r.content = malloc(sizeof(char)*(strlen(e.content)+1));
-  strcpy(r.content, e.content);
-  return r;
+  free(e.content);
+  free(e.title);
+  memset(&e, 0, sizeof(Entry)); // Sets id, title and content to zero
 }
--- wiki.h
+++ wiki.h
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 
 typedef int bool;
-#define true 1
-#define false 0
+#define true 1;
+#define false 0;
 
 typedef size_t Id;
 
@@ -15,19 +15,53 @@ typedef struct EntrySt {
 struct WikiSt;
 typedef struct WikiSt* Wiki;
 
-
+/* -------------------------
+@pre-condition:
+@post-condition: The wiki exists. (It is considered implicit that the wiki is valid.)
+@invariant:
+------------------------ */
 Wiki createWiki();
 
+/* -------------------------
+@pre-condition:
+@post-condition: The entry has been registered in the wiki.
+@invariant: The wiki exists.
+------------------------ */
 Wiki insert(Wiki w, Entry e);
 
+/* -------------------------
+@pre-condition:
+@post-condition: The entry with the selected ID has been deleted from the wiki.
+@invariant: The wiki exists.
+------------------------ */
 Wiki del(Wiki w, Id id);
 
+/* -------------------------
+@pre-condition:
+@post-condition:
+@invariant: The wiki exists.
+------------------------ */
 Entry search(Wiki w, Id id);
 
+/* -------------------------
+@pre-condition:
+@post-condition:
+@invariant: The wiki exists.
+------------------------ */
 Wiki searchTxt(Wiki w, char* txt);
 
+/* -------------------------
+@pre-condition: The wiki exists.
+@post-condition:
+@invariant:
+------------------------ */
 void destroy(Wiki w);
 
+/* -------------------------
+@pre-condition:
+@post-condition:
+@invariant: The wiki exists.
+------------------------ */
 void printWiki(Wiki w);
 
 
@@ -35,8 +69,23 @@ void printWiki(Wiki w);
 
 void printEntry(Entry e);
 
+/* -------------------------
+@pre-condition:
+@post-condition: One specific entry in the wiki has been deleted.
+@invariant: The wiki exists.
+------------------------ */
 void freeEntry(Entry e);
 
+/* -------------------------
+@pre-condition:
+@post-condition: The file has been loaded and can be used in the main.
+@invariant:
+------------------------ */
 Wiki loadFile(char* file);
 
+/* -------------------------
+@pre-condition:
+@post-condition:
+@invariant: The wiki exists.
+------------------------ */
 Entry copyEntry(Entry e);
